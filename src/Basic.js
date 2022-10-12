ad137b636419c70bb6c9d8d53fa0d0a6d1051992
ChodeBusta <115305664+ChodeBusta@users.noreply.github.com>
2022-10-11 18:34:42 -0700

    Converted to React (text doesn't work; basic)

---
+++ src/Basic.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Basic extends Component {
+    constructor(props) {
+        super(props);
+        this.states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", 
+                        "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", 
+                        "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", 
+                        "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
+                        "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
+                        "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
+                        "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                        "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+                        "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+                        "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+        this.text = "";
+    }
+
+    loopButtons() {
+        const buttons = [];
+        for (let i = 0; i < this.states.length; i++) {
+            buttons.push(this.createButtons(this.states[i]));
+        }
+        return buttons;
+    }
+
+    createButtons(name) {
+        return (
+            React.createElement(
+                "button",
+                {
+                    key: name,
+                    onClick: () => this.handleClick(name)
+                },
+                name                
+            )
+        )
+    }
+
+    handleClick(name) {
+        console.log("HERE: " + name);
+        this.text = name;
+    }
+
+    render () {
+        console.log('BRUH: ' + this.text);
+        const buttons = this.loopButtons();
+        return (
+            <div>
+                <br></br>
+                    {buttons}
+                <br></br>
+                <br></br>
+                <p>{this.text}</p>
+            </div>
+        );
+    };
+
+}
+
+export default Basic;
