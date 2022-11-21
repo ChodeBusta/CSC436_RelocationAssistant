df59fa57be8507b5addb88858f9f462ea68757c7
Loc Tran <ltran6751@gmail.com>
2022-11-21 13:57:28 -0700

    killed the basic version

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
-import Basic from './Basic';
 import Map from './Map';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.handleMap = this.handleMapClick.bind(this);
-        this.handleBasic = this.handleBasicClick.bind(this);
         this.state = {version: <Map />};
     }
-
-    handleMapClick = () => {
-        this.setState({version: <Map />});
-    };
-
-    handleBasicClick = () => {
-        this.setState({version: <Basic />});
-    };
 
     render() {
         return (
             <div>
                 <h1>Relocation Assistant</h1>
-                <button onClick={this.handleMapClick}>Map Version</button>
-                <button onClick={this.handleBasicClick}>Basic Version</button>
                 {this.state.version}
             </div>
         );
--- src/MapChart.js
+++ src/MapChart.js
@@ -42,78 +42,46 @@ class MapChart extends Component {
         this.id.push(null); this.id.push(null); this.geo.push(null); this.geo.push(null);
     }
 
-    render() {
+    clearInfo(){
+        this.leftText = "";
+        this.rightText = "";
+        this.setState({salary: ''});
+        this.forceUpdate();
+    }
+
+    showLessExpensive(){
+        console.log("Less Expensive Button clicked")
+        // TODO
+    }
+
+    showMoreExpensive(){
+        console.log("More Expensive Button clicked")
+        // TODO
+    }
+    
+    utilityButtons(){
+        const buttons = []
+        // Clear
+        buttons.push(this.createButton("Clear", () => this.clearInfo()))
+        // Shows states less expensive than the one selected
+        buttons.push(this.createButton("Show Less Expensive", () => this.showLessExpensive()))
+        // Shows states more expensive than the one selected
+        buttons.push(this.createButton("Show More Expensive", () => this.showMoreExpensive()))
+        return buttons;
+    }
+
+    createButton(buttonName, lambda) {
         return (
-            <>
-            <ComposableMap projection="geoAlbersUsa">
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) => (
-                    <>
-                    {geographies.map(geo => (
-                        <Geography
-                        id={allStates.find(s => s.val === geo.id).id}
-                        key={geo.rsmKey}
-                        stroke="#FFFFFF"
-                        geography={geo}
-                        fill="#DDDDDD"
-                        style={{
-                            default: {fill: geo.fill},
-                            hover: {fill: "#ADD8E6"},
-                            pressed: {fill: "#F9F900"},
-                        }}
-                        onClick={() => this.display(allStates.find(s => s.val === geo.id).id, geo)}
-                    />
-                ))}
-                {geographies.map(geo => {
-                    const centroid = geoCentroid(geo);
-                    const cur = allStates.find(s => s.val === geo.id);
-                    return (
-                        <g id={cur.id} key={geo.rsmKey + "-name"}>
-                            {cur &&
-                            centroid[0] > -160 &&
-                            centroid[0] < -67 &&
-                            (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                                <Marker coordinates={centroid}>
-                                    <text y="2" fontSize={14} textAnchor="middle">
-                                        {cur.id}
-                                    </text>
-                                </Marker>
-                        ) : (
-                            <Annotation
-                                subject={centroid}
-                                dx={offsets[cur.id][0]}
-                                dy={offsets[cur.id][1]}
-                            >
-                                <text x={4} fontSize={14} alignmentBaseline="middle">
-                                    {cur.id}
-                                </text>
-                            </Annotation>
-                        ))}
-                        </g>
-                    );
-                })}
-                </>
-                )}
-                </Geographies>
-            </ComposableMap>
-            <div id="mapGridContainer">
-                <div id="leftResults">
-                    <p>{this.leftText}</p>
-                </div>
-                <div id="rightResults">
-                    <p>{this.rightText}</p>
-                </div>
-                <div id="salaryComparison">
-                    <p>
-                        <input value={this.state.salary} onChange={this.validateNumber}/>
-                        &nbsp;is equivalent to a salary of&nbsp;
-                        <input value={this.compareSalary()} readOnly/>
-                    </p>
-                </div>
-            </div>
-            </>
-        );
-    };
+            React.createElement(
+                "button",
+                {
+                    key: buttonName,
+                    onClick: lambda
+                },
+                buttonName                
+            )
+        )
+    }
     
     validateNumber(e){
         const re = /^[0-9\b]+$/;
@@ -216,6 +184,82 @@ class MapChart extends Component {
             }
         )
     }
+
+    render() {
+        const utilityButtons = this.utilityButtons();
+        return (
+            <>
+            <div>{utilityButtons}</div>
+            <ComposableMap projection="geoAlbersUsa">
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) => (
+                    <>
+                    {geographies.map(geo => (
+                        <Geography
+                        id={allStates.find(s => s.val === geo.id).id}
+                        key={geo.rsmKey}
+                        stroke="#FFFFFF"
+                        geography={geo}
+                        fill="#DDDDDD"
+                        style={{
+                            default: {fill: geo.fill},
+                            hover: {fill: "#ADD8E6"},
+                            pressed: {fill: "#F9F900"},
+                        }}
+                        onClick={() => this.display(allStates.find(s => s.val === geo.id).id, geo)}
+                    />
+                ))}
+                {geographies.map(geo => {
+                    const centroid = geoCentroid(geo);
+                    const cur = allStates.find(s => s.val === geo.id);
+                    return (
+                        <g id={cur.id} key={geo.rsmKey + "-name"}>
+                            {cur &&
+                            centroid[0] > -160 &&
+                            centroid[0] < -67 &&
+                            (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                                <Marker coordinates={centroid}>
+                                    <text y="2" fontSize={14} textAnchor="middle">
+                                        {cur.id}
+                                    </text>
+                                </Marker>
+                        ) : (
+                            <Annotation
+                                subject={centroid}
+                                dx={offsets[cur.id][0]}
+                                dy={offsets[cur.id][1]}
+                            >
+                                <text x={4} fontSize={14} alignmentBaseline="middle">
+                                    {cur.id}
+                                </text>
+                            </Annotation>
+                        ))}
+                        </g>
+                    );
+                })}
+                </>
+                )}
+                </Geographies>
+            </ComposableMap>
+            <div id="mapGridContainer">
+                <div id="leftResults">
+                    <p>{this.leftText}</p>
+                </div>
+                <div id="rightResults">
+                    <p>{this.rightText}</p>
+                </div>
+                <div id="salaryComparison">
+                    <p>
+                        <input value={this.state.salary} onChange={this.validateNumber}/>
+                        &nbsp;is equivalent to a salary of&nbsp;
+                        <input value={this.compareSalary()} readOnly/>
+                    </p>
+                </div>
+            </div>
+            </>
+        );
+    };
+
 };
 
 export default MapChart;
